989e229638992a2ab7a9e6332a22d31f40ed688d
Faizankhan2911 <135813203+Faizankhan2911@users.noreply.github.com>
2023-10-30 12:17:16 +0530

    Add files via upload

---
+++ SwitchCase.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class SwitchCase {
+    Scanner sc = new Scanner(System.in);
+    int  color = sc.nextInt();
+    switch(color){  
+        case1: System.out.println("orange");break;  
+        case2: System.out.println("green");break;  
+        case3: System.out.println("red");break;  
+        case4: System.out.println("black");break;  
+        
+        }  
+    }  
+    
