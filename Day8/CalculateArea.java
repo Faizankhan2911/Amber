47a364585c39327d46e917f3d0cc307d0faf2448
Faizan Khan BJ <135813203+Faizankhan2911@users.noreply.github.com>
2023-11-14 18:56:29 +0530

    Add files via upload

---
+++ Day8/CalculateArea.java
@@ -0,0 +1,11 @@
+package Day8;
+
+public class CalculateArea implements Area{
+
+    public static void main(String[] args) {
+        Area a1 = new CalculateArea();
+        a1.circle(8);
+        
+    }
+    
+}
---
+++ Day8/CalculateSquare.java
@@ -0,0 +1,10 @@
+package Day8;
+public class CalculateSquare implements Square{
+    
+    public static void main(String[] args) {
+        Square c1 = new CalculateSquare();
+        c1.square(4);
+            
+        
+    }
+}
